e0e0c81689c7d5ae03616552ca2eaa8e0eafb730
YuChi Zhang <zhangyuchi@itiger.com>
2020-08-10 09:57:43 +0800

    拦截器管理类实现中

---
+++ src/axios.ts
@@ -0,0 +1,16 @@
+import Axios from "./core/Axios";
+import { extend } from "./helpers/util";
+import { AxiosInstance } from "./types";
+
+function createInstance(): AxiosInstance {
+    const context = new Axios()
+    const instance = Axios.prototype.request.bind(context)
+
+    extend(instance, context)
+
+    return instance as AxiosInstance
+}
+
+const axios = createInstance()
+
+export default axios;
---
+++ src/types/index.ts
@@ -0,0 +1,70 @@
+export type Method = 'get' | 'GET'
+    | 'post' | 'Post'
+    | 'delete' | 'Delete'
+    | 'head' | 'Head'
+    | 'options' | 'Options'
+    | 'patch' | 'Patch'
+    | 'put' | 'Put'
+
+export interface AxiosRequestConfig {
+    url?: string
+    method?: Method
+    data?: any
+    params?: any
+    headers?: any
+    responseType?: XMLHttpRequestResponseType
+    timeout?: number
+}
+
+export interface AxiosResponse<T=any> {
+    data: T
+    status: number
+    statusText: string
+    headers: any
+    config: AxiosRequestConfig
+    request: any
+}
+
+export interface AxiosPromise<T=any> extends Promise<AxiosResponse<T>> {
+}
+
+export interface AxiosError extends Error {
+    isAxiosError: boolean
+    config: AxiosRequestConfig
+    code?: string | null
+    request?: any
+    response?: AxiosResponse
+}
+
+export interface Axios {
+    request<T=any>(config: AxiosRequestConfig): AxiosPromise<T>
+
+    get<T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    delete<T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    head<T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    options<T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    post<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    put<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+
+    patch<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+}
+
+export interface AxiosInstance extends Axios {
+    <T=any>(config: AxiosRequestConfig): AxiosPromise<T>
+
+    <T=any>(url: string, config: AxiosRequestConfig): AxiosPromise<T>
+}
+
+export interface AxiosInterceptorManager {
+    // use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number
+}
+
+export interface ResovledFn<T> {
+    (val: T): T | Promise<T>
+}
+
